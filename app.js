fd150e68e57c7695f1ad9c025606eb573ff78bf1
alex2022cci <alex2022cci@outlook.fr>
2022-11-27 15:31:55 +0100

    version 4 boutons

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 
 const imgList = document.querySelector("#imgList");
 
-
-
 function f1(){
     /*filter: sepia(1);*/
     imgList.style.filter  = "sepia(0)";
-
 }
 
 function f2(){
     /*filter: sepia(1);*/
     imgList.style.filter  = "sepia(1)";
-
-
-
 }
+
+
+function f3(){
+    /*shadow OFF*/
+    imgList.style.filter  =  "drop-shadow(0px 0px 0px #ffffff)";
+    }
+
+function f4(){
+    /*shadow ON*/
+    imgList.style.filter  =  "drop-shadow(30px 10px 4px #4444dd)";
+    }
