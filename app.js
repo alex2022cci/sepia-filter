330ac5ed1b347d985ce60ee3e8cd2cdb294ca34d
alex2022cci <alex2022cci@outlook.fr>
2022-11-27 15:22:45 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const imgList = document.querySelector("#imgList");
+
+
+
+function f1(){
+    /*filter: sepia(1);*/
+    imgList.style.filter  = "sepia(0)";
+
+}
+
+function f2(){
+    /*filter: sepia(1);*/
+    imgList.style.filter  = "sepia(1)";
+
+
+
+}
